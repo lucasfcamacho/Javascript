341132150f467069c7308a5c0b7c3214d02d3adc
LucasFran <lcamacho107@alumnos.frh.utn.edu.ar>
2023-04-02 12:32:45 -0300

    Mejor estructuración del código

---
+++ JS/codigo.js
@@ -0,0 +1,261 @@
+const modal = document.getElementById('modal');
+        const close = document.getElementById('close');
+        const background = document.getElementById('background');
+        const modalContenido = document.getElementById('modal-contenido');
+        const loader = document.getElementById('loader');
+        const home = document.getElementById('home');
+        const form = document.getElementById('form');
+        const btnGuardar = document.getElementById('btn-guardar');
+        const btnCancelar = document.getElementById('btn-cancelar');
+        const btnCrear = document.getElementById('btn-crear');
+
+        const nombre = document.getElementById('nombre');
+        const email = document.getElementById('email');
+        const telefono = document.getElementById('telefono');
+        const web = document.getElementById('web');
+
+        const listadoUsuarios = document.getElementById('listado-usuarios');
+
+        const estado = {
+            usuarios: null
+        };
+
+        function cargarFormulario(id) {
+            nombre.value = '';
+            email.value = '';
+            telefono.value = '';
+            web.value = '';
+
+            if (id) {
+                realizarPeticion(
+                    'get',
+                    `https://jsonplaceholder.typicode.com/users/${id}`,
+                    usuario => {
+                        nombre.value = usuario.name;
+                        email.value = usuario.email;
+                        telefono.value = usuario.phone;
+                        web.value = usuario.website;
+                    }
+                    , handleError
+                );
+            }
+        }
+
+        function mostrarOcultar() {
+            console.log('mostrarOcultar')
+            const pathname = location.pathname.split('/');
+            
+
+            home.classList.remove('show');
+            form.classList.remove('show');
+
+            switch(pathname[1]) {
+                case 'home':
+                    realizarPeticion('get', 'https://jsonplaceholder.typicode.com/users', mostrarUsuarios, handleError);
+                    home.classList.add('show');
+                    break;
+                case 'crear':
+                    cargarFormulario();
+                    form.classList.add('show');
+                    break;
+                case 'modificar':
+                    cargarFormulario(pathname[2]);
+                    form.classList.add('show');
+                    break;
+            }
+        }
+
+        function showLoader(show) {
+            if (show) {
+                loader.classList.add('show');
+            } else {
+                loader.classList.remove('show');
+            }
+        }
+
+        function showForm(show) {
+            if (show) {
+                form.classList.add('show');
+                listadoUsuarios.classList.add('hide');
+                btnCrear.classList.add('hide');
+            } else {
+                form.classList.remove('show');
+                listadoUsuarios.classList.remove('hide');
+                btnCrear.classList.remove('hide');
+            }
+        }
+
+        function realizarPeticion(metodo, url, handleOk, handleError, datos) {
+            const xhr = new XMLHttpRequest();
+
+            showLoader(true);
+
+            xhr.open(metodo, url);
+
+            xhr.responseType = 'json';
+
+            xhr.addEventListener('load', e => {
+                if (xhr.status >= 200 && xhr.status <= 299) {
+                    console.log(xhr);
+                    handleOk(xhr.response);
+                } else {
+                    handleError(xhr.status);
+                }
+                showLoader(false);
+            });
+
+            xhr.addEventListener('error', e => {
+                handleError('Error');
+                showLoader(false);
+            });
+
+            xhr.send(JSON.stringify(datos));
+        }
+
+        function handleError(error) {
+            alert(error);
+        }
+
+        function mostrarUsuarios(usuarios) {
+            estado.usuarios = usuarios;
+            usuarios.forEach(mostrarUsuario)
+        }
+
+        function eliminarUsuario() {
+            const article = document.getElementById(`usuario-${estado.usuarioEliminado}`);
+
+            if (article) {
+                article.remove();
+            }
+        }
+
+        function ocultarModal() {
+            modal.classList.remove('show');
+        }
+
+        function handleEliminar(event) {
+            const id = parseInt(event.target.parentNode.id.split('-')[1]);
+            const usuario = estado.usuarios.find(u => u.id === id);
+
+            modalContenido.innerHTML = `
+                <h3>¿Esta seguro que desea eliminar el usuario ${usuario.name}?</h3>
+            `;
+
+            const btnCancelar = document.createElement('button');
+            btnCancelar.className = 'btn btn-cancel';
+            btnCancelar.textContent = 'Cancelar';
+
+            btnCancelar.addEventListener('click', e => {
+                modal.classList.remove('show');
+            });
+
+            const btnOk = document.createElement('button');
+            btnOk.className = 'btn btn-ok';
+            btnOk.textContent = 'OK';
+
+            btnOk.addEventListener('click', e => {
+                estado.usuarioEliminado = usuario.id;
+
+                realizarPeticion(
+                    'delete',
+                    `https://jsonplaceholder.typicode.com/users/${usuario.id}`,
+                    eliminarUsuario,
+                    handleError
+                );
+
+                modal.classList.remove('show');
+            });
+
+            modalContenido.append(btnCancelar, btnOk)
+
+            modal.classList.add('show');
+        }
+
+
+        function mostrarUsuario(usuario) {
+            const article = document.createElement('article');
+            article.className = 'usuario';
+            article.id = `usuario-${usuario.id}`;
+
+            article.innerHTML = `
+                <h2>${usuario.name}</h2>
+                <ul>
+                    <li>
+                        <span class="negrita">Email:</span> ${usuario.email}
+                    </li>
+                    <li>
+                        <span class="negrita">Telefono:</span> ${usuario.phone}
+                    </li>
+                    <li>
+                        <span class="negrita">Web:</span> ${usuario.website}
+                    </li>
+                </ul>
+            `;
+
+            const btnModificar = document.createElement('button');
+            btnModificar.className = 'btn btn-ok';
+            btnModificar.textContent = 'Modificar';
+
+            btnModificar.addEventListener('click', e => {
+                cambiarURL(`/modificar/${usuario.id}`, 'Modificar');
+            });
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.className = 'btn btn-cancel';
+            btnEliminar.textContent = 'Eliminar';
+
+            btnEliminar.addEventListener('click', handleEliminar);
+
+            article.append(btnModificar, btnEliminar);
+
+            listadoUsuarios.append(article);            
+        }
+
+        close.addEventListener('click', ocultarModal);
+        background.addEventListener('click', ocultarModal);
+        btnCrear.addEventListener('click', e => {
+            cambiarURL('/crear', 'Crear');
+        });
+        
+        btnCancelar.addEventListener('click', () => {
+            cambiarURL('/home', 'Home');
+        });
+        
+        btnGuardar.addEventListener('click', e => {
+            const pathname = location.pathname.split('/');
+            const metodo = pathname[1] === 'crear' ? 'post' : 'put';
+            let url = 'https://jsonplaceholder.typicode.com/users'
+
+            if (pathname[1] === 'modificar') {
+                url += `/${pathname[2]}`;
+            }
+
+            realizarPeticion(
+                metodo,
+                url,
+                () => {
+                    cambiarURL('/home', 'Home');
+                },
+                handleError,
+                {
+                    name: nombre.value,
+                    email: email.value,
+                    phone: telefono.value,
+                    website: web.value
+                }
+            );
+        });
+
+        function cambiarURL(url, title) {
+            history.pushState(null, title, url);
+            mostrarOcultar();
+        }
+
+        window.addEventListener('popstate', mostrarOcultar);
+
+        if (location.pathname === '/') {
+            cambiarURL('/home', 'Home');
+        } else {
+            cambiarURL('/home', 'Home');
+            mostrarOcultar();
+        }
